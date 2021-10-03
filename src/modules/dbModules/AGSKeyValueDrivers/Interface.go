cd914e76a24ea6e7c9de04f0fb8771879657b094
NIkolay <nikhovas@gmail.com>
2021-10-03 16:39:53 +0300

    meta

--- src/modules/dbModules/AGSKeyValueDrivers/Interface.go
+++
@@ -1,7 +0,0 @@
-package AGSKeyValueDrivers
-
-type AGSKeyValueInterface interface {
-	Get(key string) ([]byte, error)
-	SetIfNotExists(key string, data []byte) error
-	GetNewUniqueNumber() (int, error)
-}
